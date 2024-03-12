be101e6761a3f33d5a2ec13dd3b0d954a0c6fd47
martinromario55 <martinromario55@gmail.com>
2024-03-12 09:39:59 +0300

    Favourites Screen

--- src/app/(tabs)/favourites.tsx
+++ src/app/(tabs)/favourites.tsx
@@ -1,9 +1,53 @@
-import { FlatList, StyleSheet } from 'react-native'
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native'
 import { Text, View } from '@/src/components/Themed'
-import { tracks } from '@/assets/data/tracks'
+// import { tracks } from '@/assets/data/tracks'
 import TrackListItem from '@/src/components/TrackListItem'
+import { gql, useQuery } from '@apollo/client'
+import { useEffect } from 'react'
+
+const query = gql`
+  query MyQuery($userId: String!) {
+    favouritesByUserid(userid: $userId) {
+      id
+      trackid
+      userid
+      track {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`
 
 export default function FavouritesScreen() {
+  const { data, loading, error, refetch } = useQuery(query, {
+    variables: {
+      userId: 'martin',
+    },
+  })
+
+  if (loading) return <ActivityIndicator />
+  if (error) {
+    return <Text style={{ color: '#fff' }}>Failed to fetch tracks</Text>
+  }
+
+  const tracks = (data?.favouritesByUserid || []).map(fav => fav.track)
+
+  // console.log('favourites', tracks)
   return (
     <View style={styles.container}>
       <FlatList
