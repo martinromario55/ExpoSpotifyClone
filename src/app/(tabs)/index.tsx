e0b1c98e62270c1ad168b4421d74e823a540343d
martinromario55 <martinromario55@gmail.com>
2024-03-12 06:51:07 +0300

    Recommendation query

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,54 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 import React from 'react'
-import { tracks } from '@/assets/data/tracks'
+// import { tracks } from '@/assets/data/tracks'
 import TrackListItem from '@/src/components/TrackListItem'
+import { gql, useQuery } from '@apollo/client'
 
-const index = () => {
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`
+
+const HomeScreen = () => {
+  const { data, loading, error } = useQuery(query, {
+    variables: {
+      genres: 'pop',
+    },
+  })
+  if (loading) return <ActivityIndicator />
+  if (error)
+    return (
+      <Text style={{ color: '#fff' }}>Failed to fetch recommendations</Text>
+    )
+
+  // console.log('data', data)
+  const tracks = data?.recommendations?.tracks || []
   return (
     <View style={styles.container}>
       <FlatList
@@ -15,7 +60,7 @@ const index = () => {
   )
 }
 
-export default index
+export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
